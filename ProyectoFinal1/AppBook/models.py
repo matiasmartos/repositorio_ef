a7d4e6d978d2a9aeb33736a0c0571c50a8539e77
Matias Martos <martostanom@gmail.com>
2022-01-24 20:10:58 -0300

    Modelos

--- ProyectoFinal1/AppBook/models.py
+++ ProyectoFinal1/AppBook/models.py
@@ -2,3 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+class Libro(models.Model):
+
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    editorial = models.CharField(max_length=50)
+    genero = models.CharField(max_length=50)
+    anio = models.DateField()
+    reservado = models.BooleanField()
+
+class Sucursal(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    localidad = models.CharField(max_length=50)
+
+class Usuario(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nro_socio = models.IntegerField()
+    email = models.EmailField()
+
+class Empleado(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    cargo = models.CharField(max_length=30)
+      
